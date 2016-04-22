15c66b52ad8434688bacfe33101e938e8e9f9f89
mikkokotila <mailme@mikkokotila.com>
2016-04-22 17:30:10 +0100

    Update dnsignals.sh

--- dnsignals.sh
+++ dnsignals.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-## r e q u i r e m e n t s 
-
-# sudo apt-get install num-utils
-# cpan> install HTML::Strip
-
-# signals.ua (with a list of common user-agents)
-# signals.proxy (
-# master.input (with the domains to be scanned)
-
 ## S C R A P I N G
 
 user_agent()
